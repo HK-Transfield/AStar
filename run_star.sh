01f7c1a98c680914859df8b0b8bb13c353b87734
HK Transfield <h.k.transfield1@gmail.com>
2022-06-14 15:55:34 +1200

    add test cases

--- run_star.sh
+++ run_star.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
 export PATH_TO_FX=lib/ # set classpath
-csv_file="$1"
-start_index="$2"
-end_index="$3"
-d="$4"
+
 
 # compile and run program
-javac --module-path $PATH_TO_FX --add-modules javafx.controls *.java && java --module-path $PATH_TO_FX --add-modules javafx.controls Stars "$csv_file" "$start_index" "$end_index" "$d"
+javac Search.java
+javac Node.java
+javac Path.java
+javac --module-path $PATH_TO_FX --add-modules javafx.controls Stars.java
+java --module-path $PATH_TO_FX --add-modules javafx.controls Stars small_v2.csv 39 13 20
+# java --module-path $PATH_TO_FX --add-modules javafx.controls Stars small_v2.csv 22 20 17
+# java --module-path $PATH_TO_FX --add-modules javafx.controls Stars clusters_v2.csv 57 82 17
+# java --module-path $PATH_TO_FX --add-modules javafx.controls Stars clusters_v2.csv 57 82 10
+# java --module-path $PATH_TO_FX --add-modules javafx.controls Stars spiral_v2.csv 75 60 9
+# java --module-path $PATH_TO_FX --add-modules javafx.controls Stars spiral_v2.csv 118 94 40
+# java --module-path $PATH_TO_FX --add-modules javafx.controls Stars trap_v2.csv 170 230 20
+# java --module-path $PATH_TO_FX --add-modules javafx.controls Stars trap_v2.csv 136 208 15
+# java --module-path $PATH_TO_FX --add-modules javafx.controls Stars spiral_v2.csv 75 83 7
+# java --module-path $PATH_TO_FX --add-modules javafx.controls Stars small_v2.csv 32 2 10
